550f1857d2c11d9c3bb4a0ee423a812cd1633ccc
Tao <48190353+Taoisty@users.noreply.github.com>
2023-07-03 16:53:54 +0700

    menambkahan else untuk opsi value tidak ditemukan

--- src/main/kotlin/WhenExpressions.kt
+++ src/main/kotlin/WhenExpressions.kt
@@ -1,9 +1,10 @@
 fun main(){
-    val value = 7
-
-    when(value){
+    val value = 20
+    val stringOfValue = when(value){
         6 -> println("value is 6")
         7 -> println("value is 7")
         8 -> println("value is 8")
+        else -> println("value cannot be reached")
     }
+    println(stringOfValue)
 }
