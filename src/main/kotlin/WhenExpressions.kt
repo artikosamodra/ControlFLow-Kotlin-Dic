7a5e03ca15e8b29fa0f190887a3920adf5001beb
Tao <48190353+Taoisty@users.noreply.github.com>
2023-07-03 17:00:58 +0700

    Jika setiap branch memiliki lebih dari 1 kode

--- src/main/kotlin/WhenExpressions.kt
+++ src/main/kotlin/WhenExpressions.kt
@@ -1,10 +1,23 @@
 fun main(){
     val value = 20
     val stringOfValue = when(value){
-        6 -> println("value is 6")
-        7 -> println("value is 7")
-        8 -> println("value is 8")
-        else -> println("value cannot be reached")
+        6 -> {
+            println("Six")
+            "value is 6"
+        }
+        7 -> {
+            println("Seven")
+            "value is 7"
+        }
+        8 -> {
+            println("Eight")
+            "value is 8"
+        }
+
+        else -> {
+            println("undefined")
+            "value cannot be reached"
+        }
     }
     println(stringOfValue)
 }
